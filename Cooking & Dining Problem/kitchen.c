58b4c89b86fbc9650d7c3f3cf3bf8343b9ea2688
Göksel <can.onal@ozu.edu.tr>
2021-04-26 02:46:51 +0300

    OzU Restaurant

--- Cooking & Dining Problem/kitchen.c
+++ Cooking & Dining Problem/kitchen.c
@@ -1,7 +1,6 @@
 /**Göksel can Önal, S011827 
  * This is the kitchen simulation code for OzuRest.
  * French chef and 3 students from gastronomy department are preparing delicious meals in here
- * You need to solve their problems.
 **/
 #include "meal.h"
 #include <pthread.h>
@@ -25,7 +24,7 @@ struct meal Menu[4] = {
 int meal_counter = 0;
 int meal_ing_counter = 0;
 int pot_counter = 0;
-// Define all required mutexes here
+
 pthread_mutex_t glove_mutex[GLOVE_COUNT];
 pthread_mutex_t ingredient_mutex;
 pthread_mutex_t pot_mutex;
@@ -33,7 +32,6 @@ pthread_mutex_t pot_mutex;
 
 void put_gloves(int apprentice_id) {
     printf("Apprentice %d is picking gloves \n", apprentice_id);
-    // Implement a mutex lock mechanism for gloves here
     if(apprentice_id == 0){
         pthread_mutex_lock(&glove_mutex[0]);
         pthread_mutex_lock(&glove_mutex[1]);
@@ -50,8 +48,6 @@ void put_gloves(int apprentice_id) {
 }
 
 void remove_gloves(int apprentice_id) {
-    // Implement a mutex unlock mechanism for gloves here
-    
     if(apprentice_id == 0){
         pthread_mutex_unlock(&glove_mutex[0]);
         pthread_mutex_unlock(&glove_mutex[1]);
@@ -69,12 +65,10 @@ void remove_gloves(int apprentice_id) {
 
 void pick_ingredient(int apprentice_id, int* meal_index, int* ing_index) {
     put_gloves(apprentice_id);
-    // Implement a control mechanism here using mutexes if needed
     *meal_index = meal_counter;
     *ing_index = meal_ing_counter;
     printf("Apprentice %d has taken ingredient %s\n", apprentice_id, Menu[*meal_index].ingredients[*ing_index].name);
-    // Possible Race condition here, take your precaution
-
+   
    if(meal_ing_counter < 2){
         pthread_mutex_lock(&ingredient_mutex);
         meal_ing_counter = meal_ing_counter + 1;  
@@ -96,7 +90,6 @@ void prepare_ingredient(int apprentice_id, int meal_index, int ing_index) {
 
 void put_ingredient(int id, int meal_index, int ing_index) {
     while(1) {
-        // Implement a control mechanism here using mutexes to prevent the second problem mentioned in HW file
         if(pot_counter < 3){
             printf("Apprentince %d is trying to put %s into pot\n", id, Menu[meal_index].ingredients[ing_index].name);
             pthread_mutex_lock(&pot_mutex);
@@ -105,7 +98,6 @@ void put_ingredient(int id, int meal_index, int ing_index) {
             pthread_mutex_unlock(&pot_mutex);
             break;
         }
-        // Do not forget to break the loop !
     }
 }
 
@@ -128,10 +120,6 @@ void *apprentice(int *apprentice_id) {
 void *chef() {
     
     while(1) {
-        /** Chef works but you need to implement a control mechanism for him 
-         *  to prevent the second problem mentioned in HW file.
-         *  As for now, he just cooks without checking the pot.
-         */
         if(pot_counter == 3){
             printf("Chef is preparing meal %s\n", Menu[meal_counter].name);
             sleep(Menu[meal_counter].time_to_prepare);
@@ -143,8 +131,8 @@ void *chef() {
             pot_counter = 0; 
             pthread_mutex_unlock(&ingredient_mutex);
 
-            sleep(3); // Let the chef rest after preparing the meal
-        if(meal_counter == MEALS_TO_PREPARE) // If all meals are prepared chef can go home
+            sleep(3); 
+        if(meal_counter == MEALS_TO_PREPARE)
             break;
         }
     }
@@ -157,19 +145,14 @@ int main() {
 
     int apprentice_ids[APPRENTICE_COUNT] = {0 ,1 ,2};
     
-    
-    // Initialize Glove mutexes here
     for(int i = 0; i < GLOVE_COUNT; i++){
         pthread_mutex_init(&glove_mutex[i],NULL);
     }
-    // Initialize threads here
     for(int i = 0; i < APPRENTICE_COUNT; i++){
         pthread_create(&apprentice_threads[i],NULL,apprentice,&apprentice_ids[i]);
     }
-
     pthread_create(&chef_thread,NULL,chef,NULL);
 
-    // Tell the main thread to wait for other threads to complete
     for (size_t i = 0; i < APPRENTICE_COUNT; i++){
         pthread_join(apprentice_threads[i],NULL);
     }
